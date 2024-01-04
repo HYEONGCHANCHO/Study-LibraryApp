c060c15a117a34e3e478da5d043b3d030642f8f6
HYEONGCHANCHO <wjstktpghd2@naver.com>
2024-01-04 19:24:56 +0900

    transactional 및 영속성 컨텍스트 적용

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -7,6 +7,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,22 +20,25 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    //@Transactional은 아래있는 함수가 시작될 때 start transaction;을 해준다(트랜잭션을 시작)
+    //함수가 예외 없이 잘 끝났다면 commit 문제가 있다면 rollback한다.
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         User u = userRepository.save(new User(request.getName(),request.getAge()));
     }
-
+    @Transactional(readOnly = true) //select의 경우 readonly 쓰면 약간의 성능 향상가능
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new).collect(Collectors.toList());
     }
-
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
         user.updateName(request.getName());
-        userRepository.save(user);
+//        userRepository.save(user); @Transaction 때문에 영속성 컨텍스트가 가능하고, save를 명시하지 않아도 Entity가 변경을 감지해서 적용한다.
     }
-
+    @Transactional
     public void deleteUser(String name) {
 
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
