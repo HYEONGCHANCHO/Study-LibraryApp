41722757209ed3f7e830979f9633946e0b8b150a
HYEONGCHANCHO <wjstktpghd2@naver.com>
2024-01-26 21:55:14 +0900

    CalcuratorController 수정

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -11,11 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class CalculatorController {
 
-    @GetMapping("/add") //GET /add
-    public int addTwoNumbers(CalculatorAddRequest request) {
-        return request.getNumber1() + request.getNumber2();
-    }
-
+//  ㄴㄴㄴ
 
     @PostMapping("/multiply") //POST /multiply
     public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
